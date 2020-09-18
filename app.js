5ab942d80711623a166d2074c4d9574c982de8f5
Thunderbolt2-o <68735588+Thunderbolt2-o@users.noreply.github.com>
2020-09-18 13:24:33 +0530

    Simple_time_first upadates

---
+++ app.js
@@ -0,0 +1,20 @@
+setInterval(showTime, 100);
+		function showTime() {
+			let time = new Date();
+			let hour = time.getHours();
+			let min = time.getMinutes();
+			let sec = time.getSeconds();
+		  let milli = time.getMilliseconds();
+
+			hour = hour < 10 ? "0" + hour : hour;
+			min = min < 10 ? "0" + min : min;
+			sec = sec < 10 ? "0" + sec : sec;
+
+			let currentTime = hour + ":"
+				+ min + ":" + sec + ":" + milli;
+
+			document.getElementById("clock")
+				.innerHTML = currentTime;
+		}
+
+		showTime();
